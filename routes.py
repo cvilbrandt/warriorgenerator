8e5302404054740c5dac5d8d0a66be88f38f2768
cvilbrandt <60412756+cvilbrandt@users.noreply.github.com>
2021-06-18 15:03:07 -0700

    Testing...

--- routes.py
+++ routes.py
@@ -1,10 +1,4 @@
-from flask import Flask, render_template, request, redirect
-
-from data import fur, clans, descriptors, entries_list
-from generator import generator
-
-app = Flask(__name__)
-
+from app import app
 
 # Display Index Page
 @app.route('/')
---
+++ warriorapp.py
@@ -0,0 +1,1 @@
+from warriorgenerator import app
